d3707ab1d72d1218422166433d02c5e2cd006f9d
alsv06003 <alsv06003@E-PW0234JW>
2023-09-06 10:54:06 +0200

    Add project files.

---
+++ uppgift1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Exempel_uppgift_1._2
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Din dator kommer snart stängas av");
+            Console.WriteLine("Pågrund av ett fel som inträffades");
+        }
+    }
+}
